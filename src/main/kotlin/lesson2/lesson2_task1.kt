3648f8783d01e37024e56ffb56f8bab35fbb4c43
Dmitry <mistapecss@yandex.ru>
2024-09-15 14:14:22 +0300

    KS-2-1 average value

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson2
+
+fun main() {
+    var ball1: Float = 3f
+    var ball2: Float = 4f
+    var ball3: Float = 3f
+    var ball4: Float = 5f
+    var averageValue = (ball1 + ball2 + ball3 + ball4) / 4
+
+    println(averageValue)
+}
