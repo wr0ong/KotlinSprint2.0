ba55349bb4cffde7477d67b25a79fa62c01a8ea0
Dmitry <mistapecss@yandex.ru>
2024-09-15 21:26:00 +0300

    KS-1-5 constants and Yuriy Alexeevich (fixed)

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,13 +1,17 @@
 package org.example.lesson1
 
 fun main() {
-    val seconds: Int = 6480
-    var hours: Int = seconds / 3600
-    val minutes: Int = seconds / 60
-    val ostMinutes: Int = (seconds % 3600) / 60
-    val ostSeconds: Int = seconds % 60
+    val seconds: Int = GAGARINS_TIMING * SECONDS_IN_MINUTE
+    var hours: Int = seconds / (SECONDS_IN_MINUTE * SECONDS_IN_MINUTE)
+    //секунд в минутах столько же, сколько и минут в часах /\
+    val minutes: Int = seconds / SECONDS_IN_MINUTE
+    val ostMinutes: Int = (seconds % (SECONDS_IN_MINUTE * SECONDS_IN_MINUTE)) / SECONDS_IN_MINUTE
+    val ostSeconds: Int = seconds % SECONDS_IN_MINUTE
 
-    if (hours < 10) print("0$hours:") else print("$hours:")
-    if (ostMinutes < 10) print("0$ostMinutes:") else print("$ostMinutes:")
-    if (ostSeconds < 10) print("0$ostSeconds") else print(ostSeconds)
+    print(String.format("%02d:", hours))
+    print(String.format("%02d:", ostMinutes))
+    println(String.format("%02d", ostSeconds))
 }
+
+const val GAGARINS_TIMING = 108 //Минут провел Юрий Алексеевич в космосе
+const val SECONDS_IN_MINUTE = 60 //Секунд в минуте
