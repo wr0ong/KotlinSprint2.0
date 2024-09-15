bf7654a3877be971cfb1f7df03998e5a35c68c31
Dmitry <mistapecss@yandex.ru>
2024-09-15 20:33:16 +0300

    squash! KS-1-2 create and change variables (fixed)

--- src/main/kotlin/lesson1/lesson1_task2.kt
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -1,10 +1,9 @@
 package org.example.lesson1
 
 fun main() {
-    var sumOrders: Int
+    var sumOrders: Int = 75
     val thanksForBuy: String = "Спасибо за покупку"
 
-    sumOrders = 75
     println("Всего покупок - ${sumOrders}")
     println(thanksForBuy)
 
