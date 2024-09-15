f4b0f03826339154733148b6ed5bd81952b485f1
Dmitry <mistapecss@yandex.ru>
2024-09-15 13:53:00 +0300

    KS-1-2 create and change variables

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson1
+
+fun main() {
+    var sumOrders: Int
+    val thanksForBuy: String = "Спасибо за покупку"
+
+    sumOrders = 10
+    println("Всего покупок - ${sumOrders}")
+    println(thanksForBuy)
+    var sumWorkers: Int = 2000
+    //println (sumWorkers)
+    sumWorkers--
+    println(sumWorkers)
+}
