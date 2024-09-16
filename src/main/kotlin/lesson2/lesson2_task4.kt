f8d25e44b1818b18c3d1239bdffbb28100038b40
Dmitry <mistapecss@yandex.ru>
2024-09-16 09:53:36 +0300

    KS-2-4 Materials with buff (fixed)

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -3,9 +3,10 @@ package org.example.lesson2
 fun main() {
     var crystalMineral: Int = 11
     var irenMineral: Int = 7
-    val buffPower: Float = 0.2f
-    var crystalMineralFromBuff: Float = crystalMineral * buffPower
-    var irenMineralFromBuff: Float = irenMineral * buffPower
+    val buffPower: Float = 20f
+
+    var crystalMineralFromBuff: Float = crystalMineral * (buffPower / 100)
+    var irenMineralFromBuff: Float = irenMineral * (buffPower / 100)
 
     println("${crystalMineralFromBuff.toInt()}\n${irenMineralFromBuff.toInt()}")
 }
