6594ca9d94db0f27cb5f1a892bd02f08369ae9af
Dmitry <mistapecss@yandex.ru>
2024-09-16 18:39:50 +0300

    KS-1-5 task5 without comments (fixed)

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -3,7 +3,6 @@ package org.example.lesson1
 fun main() {
     val seconds: Int = GAGARINS_TIMING * SECONDS_IN_MINUTE
     var hours: Int = seconds / (SECONDS_IN_MINUTE * SECONDS_IN_MINUTE)
-    //секунд в минутах столько же, сколько и минут в часах /\
     val minutes: Int = seconds / SECONDS_IN_MINUTE
     val ostMinutes: Int = (seconds % (SECONDS_IN_MINUTE * SECONDS_IN_MINUTE)) / SECONDS_IN_MINUTE
     val ostSeconds: Int = seconds % SECONDS_IN_MINUTE
@@ -13,5 +12,5 @@ fun main() {
     println(String.format("%02d", ostSeconds))
 }
 
-const val GAGARINS_TIMING = 108 //Минут провел Юрий Алексеевич в космосе
-const val SECONDS_IN_MINUTE = 60 //Секунд в минуте
+const val GAGARINS_TIMING = 108
+const val SECONDS_IN_MINUTE = 60
