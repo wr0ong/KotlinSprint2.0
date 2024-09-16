d2d9ed799f798bf18b459a8a40da1fa58bbe17ba
Dmitry <mistapecss@yandex.ru>
2024-09-16 23:10:16 +0300

    KS-2-3 train with constant (fixed)

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -4,8 +4,10 @@ fun main() {
     var hourOfTrainStart: Int = 9
     var minuteOfTrainStart: Int = 39
     var minutesInWay: Int = 457
-    var hourOfTrainFinish: Int = (minutesInWay + 39) / 60 + 9
-    var minuteOfTrainFinish: Int = minutesInWay % 60
+    var hourOfTrainFinish: Int = (minutesInWay + minuteOfTrainStart) / MINUTES_IN_HOUR + hourOfTrainStart
+    var minuteOfTrainFinish: Int = minutesInWay % MINUTES_IN_HOUR
 
     println("$hourOfTrainFinish:$minuteOfTrainFinish")
 }
+
+const val MINUTES_IN_HOUR = 60
