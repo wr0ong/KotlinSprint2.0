b9bdacab3a24f89fbb7c17f3eaa132ea59270e81
Dmitry <mistapecss@yandex.ru>
2024-09-16 18:47:37 +0300

    KS-1-5 task5 without comments (fixed 2)

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -3,7 +3,6 @@ package org.example.lesson1
 fun main() {
     val seconds: Int = GAGARINS_TIMING * SECONDS_IN_MINUTE
     var hours: Int = seconds / (SECONDS_IN_MINUTE * SECONDS_IN_MINUTE)
-    val minutes: Int = seconds / SECONDS_IN_MINUTE
     val ostMinutes: Int = (seconds % (SECONDS_IN_MINUTE * SECONDS_IN_MINUTE)) / SECONDS_IN_MINUTE
     val ostSeconds: Int = seconds % SECONDS_IN_MINUTE
 
