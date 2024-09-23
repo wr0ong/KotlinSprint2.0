a58a12ba7e1b4566f607583235c39cc754aca1a0
Dmitry <mistapecss@yandex.ru>
2024-09-23 10:35:23 +0300

    KS-4-1 create message about access of tables

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -2,15 +2,10 @@ package org.example.lesson4
 
 fun main() {
 
-    var accessTablesToday: Int = 0
-    var accessTablesTomorrow: Int = 4
-    var accessToday: Boolean
-    var accessTomorrow: Boolean
-
-    if (TABLES_IN_CAFE <= TABLES_IN_CAFE - accessTablesToday) accessToday = false
-    else accessToday = true
-    if (TABLES_IN_CAFE <= TABLES_IN_CAFE - accessTablesTomorrow) accessTomorrow = false
-    else accessTomorrow = true
+    val accessTablesToday = 0
+    val accessTablesTomorrow = 4
+    var accessToday: Boolean = TABLES_IN_CAFE > TABLES_IN_CAFE - accessTablesToday
+    var accessTomorrow: Boolean = TABLES_IN_CAFE > TABLES_IN_CAFE - accessTablesTomorrow
 
     println("Доступность столиков на сегодня: $accessToday\nДоступность столиков на завтра: $accessTomorrow")
 }
