ecd483b3b39ba5072b2b9d94271cdfef4cb0b1fc
Dmitry <mistapecss@yandex.ru>
2024-09-24 13:13:12 +0300

    KS-5-5 create new lottery with Sets

---
+++ src/main/kotlin/lesson5/lesson5_task5.kt
@@ -0,0 +1,24 @@
+package org.example.lesson5
+
+fun main() {
+    val setOfWinningNumbers: Set<Int> = setOf((0..42).random(), (0..42).random(), (0..42).random())
+    val userChoice1: Int
+    val userChoice2: Int
+    val userChoice3: Int
+
+    println("Введите первое число от 0 до 42")
+    userChoice1 = readln().toInt()
+    println("Введите второе число от 0 до 42")
+    userChoice2 = readln().toInt()
+    println("Введите третье число от 0 до 42")
+    userChoice3 = readln().toInt()
+
+    val setOfUserChoice: Set<Int> = setOf(userChoice1, userChoice2, userChoice3)
+    if ((setOfUserChoice intersect setOfWinningNumbers).size == 0) println("Вы не угадали ни одного числа")
+    else if ((setOfUserChoice intersect setOfWinningNumbers).size == 1) println("Вы угадали 1 число, получаете утешительный приз")
+    else if ((setOfUserChoice intersect setOfWinningNumbers).size == 2) println("Вы угадали 2 числа и получаете крупный приз!")
+    else println("Вы угадали все числа и выигрываете джекпот!")
+
+    println("Сегодня победили числа ${setOfWinningNumbers.elementAt(0)}, ${setOfWinningNumbers.elementAt(1)}," +
+            " ${setOfWinningNumbers.elementAt(2)}")
+}
