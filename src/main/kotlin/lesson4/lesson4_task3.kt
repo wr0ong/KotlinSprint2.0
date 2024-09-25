4af902c09db85582caefb3dc2470bd4afc2debd7
Dmitry <mistapecss@yandex.ru>
2024-09-25 10:08:26 +0300

    KS-6-5 create checking for a bot

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -2,20 +2,19 @@ package org.example.lesson4
 
 fun main() {
     val isSunny: Boolean = true
-    val isAwningOpen: Boolean = true
+    val isTentOpen: Boolean = true
     val airHumidity: Int = 20
     val seasonOfYear: String = "Зима"
-    val favorableConditions: Boolean
+    val isFavorableConditions: Boolean
 
-    if ((isSunny == IS_SUNNY_NEED) && (isAwningOpen == IS_AWNING_OPEN_NEED) && (airHumidity == AIR_HUMIDITY_NEED) &&
-        (seasonOfYear != SEASON_OF_YEAR_NOT_NEED)) favorableConditions = true
-    else favorableConditions = false
+    isFavorableConditions = ((isSunny == IS_WEATHER_SUNNY) && (isTentOpen == IS_TENT_OPEN_NEED) &&
+            (airHumidity == AIR_HUMIDITY_NEED) && (seasonOfYear != SEASON_OF_YEAR_NOT_NEED))
 
-    println("Благоприятны ли условия сейчас для роста бобовых? $favorableConditions")
+    println("Благоприятны ли условия сейчас для роста бобовых? $isFavorableConditions")
 
 }
 
-const val IS_SUNNY_NEED: Boolean = true
-const val IS_AWNING_OPEN_NEED: Boolean = true
+const val IS_WEATHER_SUNNY: Boolean = true
+const val IS_TENT_OPEN_NEED: Boolean = true
 const val AIR_HUMIDITY_NEED: Int = 20
 const val SEASON_OF_YEAR_NOT_NEED: String = "Зима"
