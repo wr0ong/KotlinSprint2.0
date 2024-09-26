d9bea82cde91e4226e3ad5a933c33c20f83c18a0
Dmitry <mistapecss@yandex.ru>
2024-09-26 15:56:06 +0300

    KS-7-1 create password

--- src/main/kotlin/lesson7/lesson7_task1.kt
+++ src/main/kotlin/lesson7/lesson7_task1.kt
@@ -2,17 +2,20 @@ package org.example.lesson7
 
 fun main() {
     val longOfChain: Int = 2
+    val longOfPassword: Int = 6
     val mutableListOfPassword: MutableList<String> = mutableListOf()
     var randomString: String
     var randomInt: String
 
-    for (i in 1 .. LONG_OF_PASSWORD/longOfChain) {
+    for (i in 1..longOfPassword / longOfChain) {
         randomString = ('a'..'z').random().toString()
         randomInt = (1..9).random().toString()
         mutableListOfPassword.add(randomString)
         mutableListOfPassword.add(randomInt)
     }
-mutableListOfPassword.forEach{ print(it) }
+    if (longOfPassword % longOfChain != 0) {
+        randomString = ('a'..'z').random().toString()
+        mutableListOfPassword.add(randomString)
+    }
+    mutableListOfPassword.forEach { print(it) }
 }
-
-const val LONG_OF_PASSWORD = 6
