bde3804d2993b5f836c7030c909c1a58acd51e98
Dmitry <mistapecss@yandex.ru>
2024-09-27 10:37:09 +0300

    KS-4-2 create average of cargo

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -1,20 +1,18 @@
 package org.example.lesson4
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction
-
 fun main() {
     val cargoWeight1: Int = 20
     val cargoSize1: Int = 80
     val cargoWeight2: Int = 50
     val cargoSize2: Int = 100
 
-    var average1: Boolean =
+    var isAverage1: Boolean =
         (cargoWeight1 > MIN_CARGO_WEIGHT) && (cargoWeight1 <= MAX_CARGO_WEIGHT) && (cargoSize1 < MAX_CARGO_SIZE)
-    var average2: Boolean =
+    var isAverage2: Boolean =
         (cargoWeight2 > MIN_CARGO_WEIGHT) && (cargoWeight2 <= MAX_CARGO_WEIGHT) && (cargoSize2 < MAX_CARGO_SIZE)
 
-    println("Груз с весом $cargoWeight1 кг и объемом $cargoSize1 соответствует категории 'Average': $average1")
-    println("Груз с весом $cargoWeight2 кг и объемом $cargoSize2 соответствует категории 'Average': $average2")
+    println("Груз с весом $cargoWeight1 кг и объемом $cargoSize1 соответствует категории 'Average': $isAverage1")
+    println("Груз с весом $cargoWeight2 кг и объемом $cargoSize2 соответствует категории 'Average': $isAverage2")
 
 }
 const val MIN_CARGO_WEIGHT: Int = 35
