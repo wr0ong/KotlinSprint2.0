31e305e3337744cba6d03ca4f823cf8745e07231
Dmitry <mistapecss@yandex.ru>
2024-10-01 11:01:22 +0300

    KS-11-4 create recipe data classes

--- src/main/kotlin/lesson11/lesson11_task4.kt
+++ src/main/kotlin/lesson11/lesson11_task4.kt
@@ -3,11 +3,12 @@ package org.example.lesson11
 data class CategoryOfRecipe(
     val nameOfCategory: String,
     val imageOfCategory: String,
+    val recipes: String,
 )
 data class Recipe(
     val nameOfDish: String,
     val quantityOfPortion: Int,
-    val mapOfIngredients: Map<String, Int>,
+    val listOfRecipe: List<String>,
     val cookingMethod: String,
 )
 data class Ingredient(
