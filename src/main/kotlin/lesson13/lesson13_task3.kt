21df065ca4828039cf62917c2ae924cd199a13eb
Dmitry <mistapecss@yandex.ru>
2024-10-02 12:50:58 +0300

    KS-13-3 create phone directory with mList

---
+++ src/main/kotlin/lesson13/lesson13_task3.kt
@@ -0,0 +1,30 @@
+package org.example.lesson13
+
+class PhoneDirectory3(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null ?: "<не указано>"
+) {
+    fun printInstance() {
+        println("-Имя: $name\n-Номер: $phoneNumber\n-Компания: $company")
+    }
+}
+
+fun main() {
+    val mutableListOfContacts: MutableList<PhoneDirectory3> = mutableListOf()
+
+    val contact1: PhoneDirectory3 = PhoneDirectory3("Иван", 89057245325)
+    val contact2: PhoneDirectory3 = PhoneDirectory3("Степан", 89992450427)
+    val contact3: PhoneDirectory3 = PhoneDirectory3("Ростислав", 89151250505, "null")
+    val contact4: PhoneDirectory3 = PhoneDirectory3("Сергей", 89158050809, "Ростелеком")
+    val contact5: PhoneDirectory3 = PhoneDirectory3("Ибрагим", 88005353535, "Микрозайм")
+
+    mutableListOfContacts.add(contact1)
+    mutableListOfContacts.add(contact2)
+    mutableListOfContacts.add(contact3)
+    mutableListOfContacts.add(contact4)
+    mutableListOfContacts.add(contact5)
+
+    mutableListOfContacts.filter{it.company != "<не указано>"}
+        .forEach{ println(it.company) }
+}
