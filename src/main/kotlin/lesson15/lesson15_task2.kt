9487d6d72b791b70d6e9d1d788e93bae5a74c77d
Dmitry <mistapecss@yandex.ru>
2024-10-14 13:51:27 +0300

    KS-15-2 create weather server

--- src/main/kotlin/lesson15/lesson15_task2.kt
+++ src/main/kotlin/lesson15/lesson15_task2.kt
@@ -1,27 +1,27 @@
 package org.example.lesson15
 
-abstract class WeatherStationStats() {
-    open val temperature: Int = 0
-    open val precipitation: Double = 0.0
+abstract class WeatherStationStats(value: Double?) {
+    open val value: Double? = null
 }
 
-class Temperature(override val temperature: Int) : WeatherStationStats() {
+class Temperature(override val value: Double?) : WeatherStationStats(value) {
 }
 
-class PrecipitationAmount(override val precipitation: Double) : WeatherStationStats() {
+class PrecipitationAmount(override val value: Double?) : WeatherStationStats(value) {
 }
 
 class WeatherServer() {
     fun getInfo() {
-        println("Введите значение температуры или количества осадков")
+        println("Введите значение температуры (с + если температура выше 0,с  - если ниже 0) или количества осадков")
         val result = readln()
         val digit = result.partition { it.isDigit() }
-        if ((digit.second == "") || (digit.second == "-")) {
-            val temperature: Temperature = Temperature(result.toInt())
-            println("Температура: ${temperature.temperature}")
+        println(digit)
+        if ((digit.second == "-.") || (digit.second == "+.") || (digit.second == "-") || (digit.second == "+")) {
+            val temperature: Temperature = Temperature(result.toDouble())
+            println("Температура: ${temperature.value}")
         } else {
             val precipitationAmount: PrecipitationAmount = PrecipitationAmount(result.toDouble())
-            println("Осадки: ${precipitationAmount.precipitation}")
+            println("Осадки: ${precipitationAmount.value}")
         }
 
     }
