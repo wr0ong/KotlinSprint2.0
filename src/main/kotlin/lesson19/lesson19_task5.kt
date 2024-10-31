9e78b6373e4f3c3c12592acefb30d1e8524e4a3f
Dmitry <mistapecss@yandex.ru>
2024-10-31 13:41:58 +0300

    KS-19-5 create mList of peoples

--- src/main/kotlin/lesson19/lesson19_task5.kt
+++ src/main/kotlin/lesson19/lesson19_task5.kt
@@ -1,16 +1,18 @@
 package org.example.lesson19
 
-enum class Gender(){
+enum class Gender() {
     MAN,
     WOMAN
 }
 
 class People(val name: String = readln(), val gender: Gender = Gender.valueOf(readln()))
 
-fun main(){
+fun main() {
     val mListOfPeoples: MutableList<People> = mutableListOf()
-    println("Для добавления в картотеку вам необходимо: \n1) Ввести имя человека, нажать Enter\n" +
-            "2) Ввести пол человека (MAN или WOMAN), нажать Enter")
+    println(
+        "Для добавления в картотеку вам необходимо: \n1) Ввести имя человека, нажать Enter\n" +
+                "2) Ввести пол человека (MAN или WOMAN), нажать Enter"
+    )
     while (mListOfPeoples.size < 5) {
         val people: People = People(readln())
         mListOfPeoples.add(people)
