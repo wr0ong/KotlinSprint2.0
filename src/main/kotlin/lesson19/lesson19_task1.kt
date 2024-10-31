7de4dd32dd373f67dff518bc541bf1dcf82b3d4f
Dmitry <mistapecss@yandex.ru>
2024-10-31 12:22:13 +0300

    KS-19-1 create enum class of fishes

--- src/main/kotlin/lesson19/lesson19_task1.kt
+++ src/main/kotlin/lesson19/lesson19_task1.kt
@@ -1,13 +1,13 @@
 package org.example.lesson19
 
 fun main() {
-    println(
-        "Вы можете добавить в свой аквариум: ${FishOfAquarium.GUPPY.typeOfFist}, ${FishOfAquarium.GOLDFISH.typeOfFist}, " +
-                "${FishOfAquarium.ANGELFISH.typeOfFist}, ${FishOfAquarium.SIAMESE_FIGHTING_FISH.typeOfFist}"
-    )
+    var result: String = ""
+    print("Вы можете добавить в свой аквариум: ")
+    for (i in FishOfAquarium.entries) result += ("${i.typeOfFish}, ")
+    print(result.dropLast(2))
 }
 
-enum class FishOfAquarium(val typeOfFist: String) {
+enum class FishOfAquarium(val typeOfFish: String) {
     GUPPY("Guppy"),
     ANGELFISH("Angelfish"),
     GOLDFISH("Goldfish"),
