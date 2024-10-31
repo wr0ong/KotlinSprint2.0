b8ae068645e2332d70725824d1fa72b0d8c7dfca
Dmitry <mistapecss@yandex.ru>
2024-10-31 16:50:49 +0300

    KS-20-4 create list of elements

---
+++ src/main/kotlin/lesson20/lesson20_task4.kt
@@ -0,0 +1,8 @@
+package org.example.lesson20
+
+fun main() {
+    val listOfElements: List<String> = listOf("Element1", "Element2", "Element3", "Element4", "Element5", "Element6")
+    val newLambda = listOfElements.map { "Нажат элемент $it" }
+    val filteredList = newLambda.filterIndexed { index, _ -> (index % 2 != 0) }
+        .forEach { println(it) }
+}
