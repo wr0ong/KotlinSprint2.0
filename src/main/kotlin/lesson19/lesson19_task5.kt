49f0a508a0621d1775b39e9f06a88fc4f2384565
Dmitry <mistapecss@yandex.ru>
2024-11-01 15:11:33 +0300

    KS-19-5 create mList of peoples

--- src/main/kotlin/lesson19/lesson19_task5.kt
+++ src/main/kotlin/lesson19/lesson19_task5.kt
@@ -5,7 +5,7 @@ enum class Gender() {
     WOMAN
 }
 
-class People(val name: String = readln(), val gender: Gender = Gender.valueOf(readln()))
+class People(val name: String = readln(), var gender: Gender = Gender.valueOf(readln()))
 
 fun main() {
     val mListOfPeoples: MutableList<People> = mutableListOf()
@@ -13,9 +13,15 @@ fun main() {
         "Для добавления в картотеку вам необходимо: \n1) Ввести имя человека, нажать Enter\n" +
                 "2) Ввести пол человека (MAN или WOMAN), нажать Enter"
     )
+
     while (mListOfPeoples.size < 5) {
-        val people: People = People(readln())
-        mListOfPeoples.add(people)
+        val people: People
+        try {
+            people = People(readln())
+            mListOfPeoples.add(people)
+        } catch (e: IllegalArgumentException) {
+            println("Такого пола нет или вы ввели неверное значение")
+        }
     }
     println("Вы добавили в картотеку ${mListOfPeoples.size} человек: ")
     mListOfPeoples.forEach { println("${it.name} - ${it.gender}") }
