02991f92fdf90de4f6cd35b0941bd063bc8236a3
Dmitry <mistapecss@yandex.ru>
2024-11-01 14:34:35 +0300

    KS-19-4 create tank

--- src/main/kotlin/lesson19/lesson19_task4.kt
+++ src/main/kotlin/lesson19/lesson19_task4.kt
@@ -6,8 +6,7 @@ enum class Missile(val damage: Int) {
     RED(20)
 }
 
-class Tank(private val model: String) {
-    private var missileOnTank: Missile? = null
+class Tank(private val model: String, private var missileOnTank: Missile? = null) {
     fun charge(missile: Missile) {
         missileOnTank = missile
         println("Танк $model заряжен снарядом типа ${missile.name} с уроном ${missile.damage}")
@@ -19,28 +18,28 @@ class Tank(private val model: String) {
             val percentage: Int = (0..100).random()
 
             if (percentage <= 20) println("Танк $model не попал в цель!")
-            else println("Попадание! Танк $model нанес врагу ${missileOnTank!!.damage} урона!")
+            else println("Попадание! Танк $model нанес врагу ${missileOnTank?.damage ?: "0"} урона!")
         }
     }
 }
 
-    fun main() {
-        val tank1 = Tank("T-90")
-        val tank2 = Tank("T-80")
+fun main() {
+    val tank1 = Tank("T-90")
+    val tank2 = Tank("T-80")
 
-        tank1.shoot()
+    tank1.shoot()
 
-        tank1.charge(Missile.RED)
-        tank2.charge(Missile.GREEN)
-        tank1.shoot()
-        Thread.sleep(400)
-        tank2.shoot()
-        Thread.sleep(400)
-        println("Перезарядка...")
-        Thread.sleep(500)
-        tank1.charge(Missile.GREEN)
-        tank2.charge(Missile.BLUE)
-        tank1.shoot()
-        Thread.sleep(400)
-        tank2.shoot()
-    }
+    tank1.charge(Missile.RED)
+    tank2.charge(Missile.GREEN)
+    tank1.shoot()
+    Thread.sleep(400)
+    tank2.shoot()
+    Thread.sleep(400)
+    println("Перезарядка...")
+    Thread.sleep(500)
+    tank1.charge(Missile.GREEN)
+    tank2.charge(Missile.BLUE)
+    tank1.shoot()
+    Thread.sleep(400)
+    tank2.shoot()
+}
