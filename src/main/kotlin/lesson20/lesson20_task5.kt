5a231b105b07a9321466bf1102fac25332c715d3
Dmitry <mistapecss@yandex.ru>
2024-11-02 13:30:08 +0300

    KS-20-5 create robot with lambda

--- src/main/kotlin/lesson20/lesson20_task5.kt
+++ src/main/kotlin/lesson20/lesson20_task5.kt
@@ -1,22 +1,26 @@
 package org.example.lesson20
 
+
 class Robot {
+    private var modPhrase: (String) -> String = { it }
     private val mListOfPhrases: MutableList<String> = mutableListOf(
         "я робот", "хороший день сегодня", "скучно чего-то",
         "у меня нет сердца, но есть душа", "я с валли дружил когда-то"
     )
     var phrase = mListOfPhrases[(0..4).random()]
 
-    fun say(phrase: String) = println(phrase)
+    fun say() = println(modPhrase(phrase))
 
-    fun setModifier(phrase_: String) {
-        phrase = phrase.reversed()
+    fun setModifier(newModifier: (String) -> String) {
+        modPhrase = newModifier
     }
 }
 
+
 fun main() {
     val robot: Robot = Robot()
-    robot.say(robot.phrase)
-    robot.setModifier(robot.phrase)
-    robot.say(robot.phrase)
+
+    robot.say()
+    robot.setModifier({ it: String -> it.reversed() })
+    robot.say()
 }
