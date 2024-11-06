65b52b0e9bd6f9f18ad0d65dc4f09fb89311e463
Dmitry <mistapecss@yandex.ru>
2024-11-06 12:39:46 +0300

    KS-22-4 create nested data class

---
+++ src/main/kotlin/lesson22/lesson22_task4.kt
@@ -0,0 +1,71 @@
+package org.example.lesson22
+
+class ViewModel(val mainScreenState: MainScreenState) {
+    data class MainScreenState(var data: String = "", val isLoading: Boolean = false) {
+        fun loadData() {
+            println("Данных нет, начать загрузку?")
+            val startOrNot: String = readln()
+            if ((startOrNot == "Да") || (startOrNot == "да") || (startOrNot == "yes") || (startOrNot == "Yes")) {
+                println("Данные загружаются...")
+                this.copy(isLoading = true)
+                this.data += "Данные главного экрана: \n main screen: <?версия xml=\"1.0\" кодировка=\"utf-8\"?>\n" +
+                        "<LinearLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
+                        "    android:id=\"@+id/LinearLayout01\"\n" +
+                        "    android:layout_width=\"200dp\"\n" +
+                        "    android:layout_height=\"75dp\"\n" +
+                        "    android:background=\"@drawable/shape\"\n" +
+                        "    android:orientation=\"horizontal\" >\n" +
+                        "    <Кнопка android:id=\"@+id/обновить\"\n" +
+                        "        android:layout_width=\"30dp\"\n" +
+                        "        android:layout_height=\"fill_parent\"\n" +
+                        "        android:layout_alignParentBottom=\"true\"\n" +
+                        "        android:layout_alignParentLeft=\"true\""
+                Thread.sleep(1500)
+                this.data += "android:text=\"Обновить\"\n" +
+                        "        android:textSize=\"3pt\" />\n" +
+                        "    <LinearLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
+                        "        android:id=\"@+id/LinearLayout01\"\n" +
+                        "        android:layout_width=\"wrap_content\"\n" +
+                        "        android:layout_height=\"fill_parent\"\n" +
+                        "        android:background=\"@drawable/shape\"\n" +
+                        "        android:orientation=\"vertical\" >\n" +
+                        "        <TextView android:id =\"@+id/ssid\"\n" +
+                        "            android:layout_width=\"wrap_content\"\n" +
+                        "            android:layout_height=\"wrap_content\"\n" +
+                        "            android:gravity=\"center_horizontal\"\n" +
+                        "            android:text=\"SSID\"\n" +
+                        "            android:textColor=\"#333333\"\n" +
+                        "            android:textSize=\"5pt\"\n" +
+                        "            android:layout_weight=\"1\" />"
+                Thread.sleep(1500)
+                this.data += "<TextView android:id=\"@+id/wifiip\"\n" +
+                        "            android:layout_width=\"wrap_content\"\n" +
+                        "            android:layout_height=\"wrap_content\"\n" +
+                        "            android:gravity=\"center_horizontal\"\n" +
+                        "            android:text=\"IP-адрес\"\n" +
+                        "            android:textColor=\"#333333\"\n" +
+                        "            android:textSize=\"5pt\"\n" +
+                        "            android:layout_weight=\"1\" />\n" +
+                        "        <TextView android:id=\"@+id/extip\"\n" +
+                        "            android:layout_width=\"wrap_content\"\n" +
+                        "            android:layout_height=\"wrap_content\"\n" +
+                        "            android:layout_below=\"@+id/ssid\"\n" +
+                        "            android:text=\"WAN IP\"\n" +
+                        "            android:textColor=\"#333333\"\n" +
+                        "            android:textSize=\"5pt\"\n" +
+                        "            android:layout_weight=\"1\" />\n" +
+                        "    </LinearLayout>\n" +
+                        "</LinearLayout>"
+                println("Данные загружены!")
+                this.copy(isLoading = false)
+            }
+        }
+    }
+}
+
+fun main() {
+    val mainScreenState = ViewModel.MainScreenState()
+    val viewModel: ViewModel = ViewModel(mainScreenState)
+
+    viewModel.mainScreenState.loadData()
+}
